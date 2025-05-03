bac1ce8c48b300d6fe5976f7c3660f4d46f5d38b
ivankola <96351955+ivankola2007@users.noreply.github.com>
2025-05-03 11:11:38 +0300

    Lesson

--- rubocoge/Assets/Scripts/Controller.cs
+++ rubocoge/Assets/Scripts/Controller.cs
@@ -1,26 +1,67 @@
 using UnityEngine;
-using UnityEngine.Input
+using UnityEngine.InputSystem;
 
-public class Controller
+public class Controller : MonoBehaviour
 {
     [SerializeField]
-    private PlayerInputActions inputActions;
+    private Playerinputactions inputActions;
     [SerializeField]
     private CharacterController cController;
     [SerializeField]
     private Animator animator;
+    private PhotonView pView;
 
-    privte Vector2 movementInput;
+    private Vector2 movementInput;
     private Vector3 currentMovement;
-    private Quanternition rotateDir;
+    private Quaternion rotateDir;
     private bool isRun;
-    private bool isWalk
+    private bool isWalk;
+    private float rotateSpeed;
+
+    private void Awake()
+    {
+        inputActions = new Playerinputactions();
+        inputActions.CharacterController.movement.started += OnMovementAction;
+        inputActions.CharacterController.movement.performed += OnMovementAction;
+        inputActions.CharacterController.movement.canceled += OnMovementAction;
+        inputActions.CharacterController.Run.started +=  OnRun;
+        inputActions.CharacterController.Run.canceled +=  OnRun;        
+         }
+
+    private void AnimateControl()
+    {
+        animator.SetBool("isWalk",isWalk);
+        animator.SetBool("isWalk",isWalk);
+    }
+    private void PlayerRotate()
+    {
+        if (!isWalk) return;
+        rotateDir = Quaternion.Lerp(transform.rotation,Quaternion.LookRotation(currentMovement), Time.deltaTime * rotateSpeed);
+        transform.rotation = rotateDir;
+    }
     
-    private voif OnMovementAction(InputAction.CallbackContext context)
+    private void OnMovementAction(InputAction.CallbackContext context)
     {
         movementInput = context.ReadValue<Vector2>();
         currentMovement.x = movementInput.x;
         currentMovement.z = movementInput.y;
         isWalk = movementInput.x !=0 || movementInput.y !=0;
     }
+    private void OnEnable()
+    {
+        inputActions.CharacterController.Enable();
+    }
+    private void OnDisable()
+    {
+        inputActions.CharacterController.Disable();
+    }
+    private void FixedUpdate()
+    {
+        cController.Move(currentMovement * Time.fixedDeltaTime);
+    }
+ private void OnRun(InputAction.CallbackContext context)
+ {
+ isRun = context.ReadValueAsButton();
+ }
+    
 }
